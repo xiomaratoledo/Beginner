1842222bbe39b9b43ce67d84752964e36254538c
xiomaratoledo <138639501+xiomaratoledo@users.noreply.github.com>
2023-07-06 08:41:02 +0800

    Create SimpleCombination.java

---
+++ SimpleCombination.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {        
+        String name = "galactic hunter";
+        String learning  = "java";
+        //write your code below
+        String combinedString = name + " %s " + learning;
+        System.out.printf(combinedString, "learning");
+    }
+}
