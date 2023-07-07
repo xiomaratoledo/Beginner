3817971a216f69c288e0042bf13cc4fb34e99317
xiomaratoledo <138639501+xiomaratoledo@users.noreply.github.com>
2023-07-07 10:14:39 +0800

    Create Newapproach.java

---
+++ Newapproach.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String oldString = "\"Why do programmers prefer dark mode?\" she asked.\n" +
+                "\"Because light attracts bugs!\" I replied.";
+
+        String youngAndStrongTextBlock = """
+            "Why do programmers prefer dark mode?" she asked.
+            "Because light attracts bugs!" I replied.
+            """; 
+        //write the code here using text block
+
+        System.out.println(youngAndStrongTextBlock);
+    }
+}
