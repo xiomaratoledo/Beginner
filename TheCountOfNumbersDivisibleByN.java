9630844a401c140eb015665b0c0c965d6dd1bae0
xiomaratoledo <138639501+xiomaratoledo@users.noreply.github.com>
2023-07-11 05:33:54 +0800

    Create TheCountOfNumbersDivisibleByN.java

---
+++ TheCountOfNumbersDivisibleByN.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+        int count = 0;
+        for (int i = a; i <= b; i++) {
+            if (i % n == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
