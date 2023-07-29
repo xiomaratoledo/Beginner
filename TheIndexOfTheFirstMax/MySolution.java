ca8bb0fe27e6f64068ae633a99b35c51d0f40a62
xiomaratoledo <138639501+xiomaratoledo@users.noreply.github.com>
2023-07-29 17:17:51 +0800

    Create MySolution.java

---
+++ TheIndexOfTheFirstMax/MySolution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int[] myArray = new int[length]; 
+        int max = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < length; i++) {
+            myArray[i] = scanner.nextInt();
+            if (myArray[i] > max) {
+                max = myArray[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println(maxIndex);
+    }
+}
